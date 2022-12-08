189d346a7037d9a8c5a027217e2e65162c1749f4
Gyan Prakash Singh <gyangps1998@gmail.com>
2022-12-08 18:24:22 +0530

    gyan solution

--- solution.js
+++ solution.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+    let str = str.split("");
+    let l = 0, r = str.length;
+    while(l<r){
+        if(str[l]!==str[r]){
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 }
 module.exports = palindrome
